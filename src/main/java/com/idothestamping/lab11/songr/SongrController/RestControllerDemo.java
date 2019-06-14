575f0aa439600184452751d7e2410484eace0308
idothestamping <douglas.klemp@gmail.com>
2019-06-14 15:19:15 -0700

    album lab

--- src/main/java/com/idothestamping/lab11/songr/SongrController/RestControllerDemo.java
+++ src/main/java/com/idothestamping/lab11/songr/SongrController/RestControllerDemo.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SongrController {
+public class RestControllerDemo {
 
     // route for this method
     @GetMapping("/hello")
